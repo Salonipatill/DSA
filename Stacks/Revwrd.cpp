bcb691fefeabd835548cc8a12173e3f6b5aa566a
Salonipatil <salonipatil0528@gmail.com>
2026-02-04 00:06:35 +0530

    Reverse individual Words

---
+++ Stacks/Revwrd.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string reverseWords(string str){
+    stack<char> st;
+    string result = "";
+
+    for(int i = 0; i<str.length(); ++i){
+        if(str[i]!= ' '){
+            st.push(str[i]);
+        }else{
+            while (!st.empty()){
+                result += st.top();
+                st.pop();
+            }
+            result += " ";
+        }
+    }
+
+    while(!st.empty()){
+        result += st.top();;
+        st.pop();
+    }
+    return result;
+
+    }
+
+int main(){
+    string str = "Saloni Patil";
+    string reversedString = reverseWords(str);
+    cout << reversedString << endl;
+    return 0;
+}
