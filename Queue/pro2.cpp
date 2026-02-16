2ec6214b1e28206a720ce717ed53e144afa9bc5a
Salonipatil <salonipatil0528@gmail.com>
2026-02-16 23:29:45 +0530

    Queue

---
+++ Queue/pro2.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+
+#define SIZE 5
+
+class CircularQueue{
+private :
+    int arr[SIZE];
+    int front, rear;
+
+public:
+    CircularQueue(){
+        front = -1;
+        rear = -1;
+    }
+
+    void enqueue(int value){
+        if(front == (rear + 1) % SIZE){
+            cout<< "Queue is Full\n";
+            return;
+        }
+
+        if(front == -1){
+            front = rear = 0;
+        }else{
+            rear = (rear + 1)% SIZE;
+
+        }
+
+        arr[rear] = value;
+        cout<< value << "inserted\n";
+    }
+
+    void dequeue(){
+        if(front == -1){
+            cout<< "Queue is Empty\n";
+            return;
+        }
+
+        cout<<arr[front]<<"deleted\n";
+
+        if(front == rear){
+            front = rear = -1;
+        }else{
+            front = (front + 1) % SIZE;
+        }
+    }
+
+void display(){
+    if(front == -1){
+        cout<<"Queue is Empty\n";
+        return;
+    }
+
+    cout<<"Queue elements:";
+    int i = front;
+
+    while(true){
+        cout<<arr[i]<<" ";
+        if(i == rear)
+           break;
+        i = (i+1)%SIZE;
+        }
+        cout<<endl;
+}
+};
+
+int main(){
+    CircularQueue q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.display();
+
+    q.dequeue();
+    q.display();
+
+    q.enqueue(40);
+    q.enqueue(50);
+    q.enqueue(60);
+    q.display();
+
+    return 0 ;
+
+}
