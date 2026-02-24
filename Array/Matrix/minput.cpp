0a5910e0b2028370c8cf0fe6573456d224e85798
Salonipatil <salonipatil0528@gmail.com>
2026-02-24 10:46:06 +0530

    matrix

---
+++ Array/Matrix/minput.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[2][2];
+
+cout<<"Enter matrix elements:"<<endl;
+
+for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+        cin>>arr[i][j];
+    }
+}
+
+cout<<"[";
+for(int i=0; i<2; i++){
+    for(int j=0; j<2; j++){
+        cout<<arr[i][j]<<",";
+    }
+    if(i<1) cout<<endl;
+}
+cout<<"]";
+
+    return 0;
+}
