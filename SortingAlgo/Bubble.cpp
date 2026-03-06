acb74dab9944794eeab0b80b3e8296f0d9f2bc0b
Salonipatil <salonipatil0528@gmail.com>
2026-03-06 13:18:33 +0530

    Bubble Sort

---
+++ SortingAlgo/Bubble.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5] = {2,4,3,1,5};
+    
+    for(int j = 0; j<5; j++)
+    {
+        for(int i = 0; i<5-j-1; i++)
+        {
+            if(arr[i] > arr[i+1])
+            {
+                 int temp;
+                 temp = arr[i];
+                 arr[i]=arr[i+1];
+                 arr[i+1]=temp;
+            }
+        }
+    }
+
+for(int i=0; i<5; i++)
+{
+    cout<<arr[i]<<",";
+}
+return 0;
+
+}
+
