e497e8de0df217ed4d38619da3774fa34e17b4ef
Salonipatil <salonipatil0528@gmail.com>
2026-03-29 11:19:46 +0530

    Leetcode

---
+++ Leetcode/Leetcode1/discount.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<sstream>
+#include<iomanip>
+
+using namespace std;
+class Solution{
+    public:
+        string discountPrices(string sentence, int discount){
+            stringstream ss(sentence);
+            string word;
+            string result = "";
+
+             
+            while(ss >>word){
+                if(word[0] == '$'&& word.length()>1){
+                  bool isPrice = true;
+
+                  for(int i=1; i<word.length(); i++){
+                    if(!isdigit(word[i])){
+                        isPrice = false;
+                        break;
+                    }
+                  }
+
+                  if (isPrice){
+                    double price = stod(word.substr(1));
+
+                    double newPrice = price * (100 - discount)/100.0;
+
+                    stringstream temp;
+                    temp << fixed << setprecision(2) << newPrice;
+
+                    word = "$"+temp.str();
+
+                  }
+                }
+
+                if(!result.empty()) result += " ";
+                result += word;
+            }
+
+            return result;
+
+        }
+
+};
+
+
+int main(){
+    Solution sol;
+
+    string sentence1 = "I have $100 and $50";
+    int discount1 = 20;
+
+cout<<"input:"<<sentence1<<", Discount:"<<discount1
+<<"%"<<endl;
+cout<<"output:"<<sol.discountPrices(sentence1, discount1)<<endl<<endl;
+
+string sentence2 = "This book costs $3o";
+int discount2 = 10;
+cout<<"Input"<<sentence2<<", Discount:"<<discount2
+<<"%"<<endl;
+cout<<"Output:"<<sol.discountPrices(sentence2, discount2)<<endl<<endl;
+
+
+return 0;
+}
---
+++ Leetcode/Leetcode1/discount_prices.cpp
@@ -0,0 +1,17 @@
+//Convert number to string
+#include<iostream>
+#include<sstream>
+using namespace std;
+
+int main(){
+    int num = 123;
+
+    stringstream ss;
+    ss << num;
+
+    string str = ss.str();
+    
+    cout<<"String:"<<str<<endl;
+
+    return 0;
+}
