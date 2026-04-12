b5d0d541d608275883a516c2bf8e8e810e5ef957
Salonipatil <salonipatil0528@gmail.com>
2026-04-12 23:40:43 +0530

    Leetcode

---
+++ Leetcode/Leetcode1/climbstairs.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+    public:
+    int climbStairs(int n){
+        if(n<=2)return n;
+
+        int a = 1;
+        int b = 2;
+
+        for(int i = 3; i<=n; i++){
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
+
+int main(){
+    Solution obj;
+
+    int n;
+    cout<<"Enter number of steps:";
+    cin>>n;
+
+    cout<<"Number of ways to climb stairs:"
+    <<obj.climbStairs(n)<<endl;
+
+    return 0;
+}
